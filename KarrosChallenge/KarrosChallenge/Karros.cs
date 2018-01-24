446ba74ca6b697164e8eae7a50ec2ab5289cb027
Diep <lewin.yue@gmail.com>
2018-01-24 21:44:43 +0700

    0124

---
+++ KarrosChallenge/KarrosChallenge/Karros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+
+namespace KarrosChallenge
+{
+    [TestClass]
+    public class Karros
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
